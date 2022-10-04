28c8295e95a02891d70ba7ed4da2281577d6ab69
glinda93 <72871001+glinda93@users.noreply.github.com>
2022-10-04 13:41:11 +0800

    fix query log config

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ const config = {
     process.env.FROM_BLOCK == null ? 0 : Number(process.env.FROM_BLOCK),
   db: {
     connectionUrl: process.env.POSTGRES_URL || "",
-    log: process.env.QUERY_LOG == null,
+    log: process.env.QUERY_LOG != null,
   },
   alchemyApiKey: process.env.ALCHEMY_API_KEY || "",
 } as const;
